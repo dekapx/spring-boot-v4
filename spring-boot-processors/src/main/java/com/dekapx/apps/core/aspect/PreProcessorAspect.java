b626ad177e96b67399164f49079236055d3b36c5
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-10-10 15:58:12 +0100

    code clean up

--- spring-boot-processors/src/main/java/com/dekapx/apps/core/aspect/PreProcessorAspect.java
+++ spring-boot-processors/src/main/java/com/dekapx/apps/core/aspect/PreProcessorAspect.java
@@ -7,11 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
-@Configuration
+@Component
 @RequiredArgsConstructor
 public class PreProcessorAspect {
     private final PreProcessorHandler preProcessorHandler;
