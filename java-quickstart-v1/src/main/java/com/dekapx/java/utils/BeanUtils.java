30036a96f7c7a709f24732ed535a3968d04ad900
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-10-11 12:16:15 +0100

    code clean up

--- java-quickstart-v1/src/main/java/com/dekapx/java/utils/BeanUtils.java
+++ java-quickstart-v1/src/main/java/com/dekapx/java/utils/BeanUtils.java
@@ -6,7 +6,13 @@ import java.beans.Introspector;
 
 @UtilityClass
 public class BeanUtils {
+    /**
+     * Generate Spring bean name for the given class type, can be used for bean lookup purpose.
+     *
+     * @param clazz Class type
+     * @return Spring bean name
+     */
     public static String generateBeanName(Class clazz) {
-     return Introspector.decapitalize(clazz.getSimpleName());
+        return Introspector.decapitalize(clazz.getSimpleName());
     }
 }
