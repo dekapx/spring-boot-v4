20c7451329a6b0b2fa90ca15bca5a01accb97796
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-10-17 11:25:59 +0100

    code clean up

--- api-gateway/src/main/java/com/dekapx/apps/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/dekapx/apps/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.dekapx.apps.api_gateway;
+package com.dekapx.apps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ api-gateway/src/main/java/com/dekapx/apps/config/ApiGatewayFilter.java
@@ -0,0 +1,21 @@
+package com.dekapx.apps.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Configuration
+public class ApiGatewayFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        log.info("Authorization Header: [{}]", request.getHeaders().getFirst("Authorization"));
+        return chain.filter(exchange);
+    }
+}
