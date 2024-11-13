a9feed05111d9e9c6c4101d1b8188783fc629d68
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-11-13 23:16:29 +0000

    code clean up

---
+++ spring-boot-integration-v1/src/main/java/com/dekapx/apps/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.dekapx.apps.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.dekapx.apps.common.ResourceUrls.BASE_URL;
+import static com.dekapx.apps.common.ResourceUrls.INFO_URL;
+
+@Slf4j
+@RestController
+@RequestMapping(BASE_URL)
+public class TestController {
+    @GetMapping(INFO_URL)
+    public String getInfo() {
+        log.info("Service APIs up & running...");
+        return "Spring Boot Integration Demo v1.0";
+    }
+}
