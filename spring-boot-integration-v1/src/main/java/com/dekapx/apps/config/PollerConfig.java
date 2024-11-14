4525f119d1dc4cbaaf6fc6c5cffd8aff87171dae
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-11-14 23:41:57 +0000

    code clean up

--- spring-boot-integration-v1/src/main/java/com/dekapx/apps/config/PollerConfig.java
+++ spring-boot-integration-v1/src/main/java/com/dekapx/apps/config/PollerConfig.java
@@ -1,16 +1,14 @@
 package com.dekapx.apps.config;
 
 
-import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.config.EnableIntegration;
 
+@Slf4j
 @Configuration
+@EnableIntegration
+@RequiredArgsConstructor
 public class PollerConfig {
-    @Bean
-    public IntegrationFlow pollerFlow() {
-        return IntegrationFlow
-                .from()
-                .get();
-    }
 }
