cded5a9f0ed4e54b1123f648a143f53614015b5b
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-11-23 16:46:26 +0000

    code refactoring

--- service-locator-factory/src/test/java/com/dekapx/apps/factory/FileWriterFactoryITest.java
+++ service-locator-factory/src/test/java/com/dekapx/apps/factory/FileWriterFactoryITest.java
@@ -3,10 +3,20 @@ package com.dekapx.apps.factory;
 
 import com.dekapx.apps.model.FileType;
 import com.dekapx.apps.writer.CsvFileWriter;
-import org.junit.jupiter.api.Test;
+import com.dekapx.apps.writer.FileWriter;
+import com.dekapx.apps.writer.TextFileWriter;
+import com.dekapx.apps.writer.XmlFileWriter;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.stream.Stream;
+
+import static com.dekapx.apps.model.FileType.CSV;
+import static com.dekapx.apps.model.FileType.TEXT;
+import static com.dekapx.apps.model.FileType.XML;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -14,12 +24,20 @@ public class FileWriterFactoryITest {
     @Autowired
     private FileWriterFactory fileWriterFactory;
 
-    @Test
-    public void givenBeanNameShouldLookupAndReturnFileWriter() {
-        var fileWriter = this.fileWriterFactory.getFileWriter(FileType.CSV.getFactoryBeanName());
-        assertThat(fileWriter).isNotNull()
-                .satisfies(o -> {
-                    assertThat(o).isInstanceOf(CsvFileWriter.class);
-                });
+    @ParameterizedTest
+    @MethodSource("fileWriterProvider")
+    public void shouldReturnFileWriterInstanceForGivenFileType(FileType type, Class<? extends FileWriter> expectedClass) {
+        FileWriter fileWriter = this.fileWriterFactory.getFileWriter(type.getFactoryBeanName());
+        assertThat(fileWriter)
+                .isNotNull()
+                .isInstanceOfAny(expectedClass);
+    }
+
+    private static Stream<Arguments> fileWriterProvider() {
+        return Stream.of(
+                Arguments.of(CSV, CsvFileWriter.class),
+                Arguments.of(TEXT, TextFileWriter.class),
+                Arguments.of(XML, XmlFileWriter.class)
+        );
     }
 }
