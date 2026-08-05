1e49028770cb999a609d23d258ab1e2468c0c35f
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro.local>
2026-08-05 23:13:58 +0100

    code cleanup and refactoring

--- order-agent-app-v1/src/main/java/com/dekapx/apps/config/ChatClientConfig.java
+++ order-agent-app-v1/src/main/java/com/dekapx/apps/config/ChatClientConfig.java
@@ -1,6 +1,7 @@
 package com.dekapx.apps.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +12,7 @@ public class ChatClientConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
         return builder
+                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .defaultSystem(ORDER_AGENT_PROMPT)
                 .build();
     }
