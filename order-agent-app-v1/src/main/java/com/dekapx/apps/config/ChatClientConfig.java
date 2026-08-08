af4f2e5809ef7b6d2f39ddb4c257864191784cf1
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro.local>
2026-08-08 23:32:54 +0100

    code cleanup and refactoring

--- order-agent-app-v1/src/main/java/com/dekapx/apps/config/ChatClientConfig.java
+++ order-agent-app-v1/src/main/java/com/dekapx/apps/config/ChatClientConfig.java
@@ -1,19 +1,14 @@
 package com.dekapx.apps.config;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static com.dekapx.apps.common.Prompts.ORDER_AGENT_PROMPT;
 
 @Configuration
 public class ChatClientConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
         return builder
-                .defaultAdvisors(new SimpleLoggerAdvisor())
-                .defaultSystem(ORDER_AGENT_PROMPT)
                 .build();
     }
 }
--- order-agent-app-v1/src/main/java/com/dekapx/apps/service/OrderAgentService.java
+++ order-agent-app-v1/src/main/java/com/dekapx/apps/service/OrderAgentService.java
@@ -1,8 +1,10 @@
 package com.dekapx.apps.service;
 
+import com.dekapx.apps.advisor.TokenCountAdvisor;
 import jakarta.validation.constraints.NotBlank;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.stereotype.Service;
 
 import static com.dekapx.apps.common.Prompts.ORDER_AGENT_PROMPT;
@@ -20,8 +22,9 @@ public class OrderAgentService {
 
     public String ask(@NotBlank(message = "Message should not be blank") String message) {
         return chatClient
-//                .prompt(ORDER_AGENT_PROMPT)
-                .prompt()// can be overridden in ChatClientConfig as defaultSystem
+                .prompt(ORDER_AGENT_PROMPT)
+                .advisors(new SimpleLoggerAdvisor())
+                .advisors(new TokenCountAdvisor())
                 .user(message)
                 .tools(orderAgentTools)
                 .call()
