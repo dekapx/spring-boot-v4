2bf789452a57fd8767fa239d19cce233fa7c0974
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro.local>
2026-08-09 22:21:24 +0100

    code cleanup and refactoring

--- order-agent-app-v1/src/main/java/com/dekapx/apps/controller/OrderAgentController.java
+++ order-agent-app-v1/src/main/java/com/dekapx/apps/controller/OrderAgentController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/agent")
 @RequiredArgsConstructor
 public class OrderAgentController {
-
     private final OrderAgentService orderAgentService;
 
     @PostMapping("/chat")
--- order-agent-app-v1/src/main/java/com/dekapx/apps/service/OrderAgentService.java
+++ order-agent-app-v1/src/main/java/com/dekapx/apps/service/OrderAgentService.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,11 +27,28 @@ public class OrderAgentService {
     public String ask(@NotBlank(message = "Message should not be blank") String message) {
         return chatClient
                 .prompt(ORDER_AGENT_PROMPT)
+                .options(buildOllamaOptions())
                 .advisors(getAdvisors())
                 .user(message)
                 .tools(orderAgentTools)
                 .call()
                 .content();
+    }
+
+
+    /**
+     * Build and return the OllamaOptions as chat Options for Ollama.
+     * Best practice is to configure these options in the application.yml file
+     *
+     * @return object of <code>OllamaOptions</code>
+     */
+    private OllamaOptions buildOllamaOptions() {
+        return OllamaOptions.builder()
+                .model("gemma4:31b")
+                .temperature(0.7)
+                .topP(0.9)
+                .topK(40)
+                .build();
     }
 
     private List<Advisor> getAdvisors() {
